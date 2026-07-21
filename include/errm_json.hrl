a14ed540ff4a908fc55e93649983b32636d9864c
h4rl <h4rldev@proton.me>
2026-07-21 16:43:07 +0200

    fix: actually make order binary too

--- include/errm_json.hrl
+++ include/errm_json.hrl
@@ -3,5 +3,5 @@
 
 -type json_term() :: null | true | false | number() | binary() | list() | atom() | [json_term()] | #{binary() => json_term()} | #{}.
 -type parse_options() ::  #{return_strings => boolean(), max_depth => non_neg_integer()}.
--type encode_options() :: #{pretty => boolean(), indent => integer(), order => [atom()]}.
+-type encode_options() :: #{pretty => boolean(), indent => integer(), order => [atom() | binary()]}.
 -endif.
